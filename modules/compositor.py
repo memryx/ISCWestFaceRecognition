91c6c29d8ad12f6d3f3d92a337f7ae81f5306fb1
Jacob Botimer <jake@memryx.com>
2025-03-27 13:41:39 -0400

    More colors!

--- modules/compositor.py
+++ modules/compositor.py
@@ -8,7 +8,11 @@ from PySide6.QtWidgets import QCheckBox
 
 from .utils import Framerate
 
-MXBLUE = (56, 172, 206)
+class MxColors: 
+    LightBlue = (198, 234, 242)
+    Blue = (53, 169, 188)
+    DarkBlue =(20, 53, 85) 
+    Teal = (60, 187, 187)
 
 class Compositor(QObject):
     frame_ready = Signal(np.ndarray)
@@ -43,13 +47,17 @@ class Compositor(QObject):
     def update_mouse_pos(self, pos):
         self.mouse_position = pos
 
-    def draw_objects(self, frame, tracked_objects):
+    def draw_name(self, frame, obj):
+        (left, top, right, bottom) = obj.bbox
+        label = f'{obj.name}({obj.track_id})'
+        cv2.putText(frame, label, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.85, MxColors.Blue, 2)
 
+    def draw_objects(self, frame, tracked_objects):
         for obj in tracked_objects:
             (left, top, right, bottom) = obj.bbox
 
             label = f'{obj.name}({obj.track_id})'
-            cv2.putText(frame, label, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.85, MXBLUE, 2)
+            cv2.putText(frame, label, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.85, MxColors.Blue, 2)
             if self.distance_checkbox.isChecked():
                 for i, (name, distance) in enumerate(obj.distances):
                     if i == 3:
@@ -58,12 +66,12 @@ class Compositor(QObject):
                     cv2.putText(frame, label, (left + 10, top + 10 + 20 * i), cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 255, 0), 1)
 
             if self.bbox_checkbox.isChecked(): 
-                cv2.rectangle(frame, (left, top), (right, bottom), MXBLUE, 2)
+                cv2.rectangle(frame, (left, top), (right, bottom), MxColors.Blue, 2)
 
             # Draw keypoints if checkbox is checked
             if self.keypoints_checkbox.isChecked():
                 for (x, y) in obj.keypoints:
-                    cv2.circle(frame, (x, y), 5, MXBLUE, -1)
+                    cv2.circle(frame, (x, y), 5, MxColors.LightBlue, -1)
 
             # Draw semi-transparent rectangle if mouse is inside bounding box
             if self.mouse_position:
@@ -71,7 +79,7 @@ class Compositor(QObject):
                 if left <= mouse_x <= right and top <= mouse_y <= bottom:
                     overlay = frame.copy()
                     alpha = 0.5  # Transparency factor
-                    cv2.rectangle(overlay, (left, top), (right, bottom), MXBLUE, -1)
+                    cv2.rectangle(overlay, (left, top), (right, bottom), MxColors.Blue, -1)
                     # Apply the overlay
                     cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
         return frame
